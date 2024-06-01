71126ed09acdfe00f5066d8157c11f499d63a48b
cookierw <seanrwhitley@gmail.com>
2024-05-31 18:09:49 -0700

    Initial Commit

---
+++ Queen-IOKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Queen-IOKit
+//
+//  Created by Cookie on 5/31/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
