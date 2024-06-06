c8e103888e581535de2d6b7f29bb1ed41e4fdbde
cookierw <seanrwhitley@gmail.com>
2024-06-06 14:51:45 -0700

    Remove MVPCStruct dependency in favor of a 'Swiftier' solution. Tests not passing yet but builds ok

--- Queen-IOKit/Util/StringProtocol+Queen.swift
+++ Queen-IOKit/Util/StringProtocol+Queen.swift
@@ -21,8 +21,7 @@ extension StringProtocol {
         return stride(from: 0, to: count, by: 2).compactMap { _ in
             let endIndex = index(startIndex, offsetBy: 2, limitedBy: self.endIndex) ?? self.endIndex
             defer { startIndex = endIndex }
-            return UInt8(self[startIndex..<endIndex], radix: 16
-            )
+            return UInt8(self[startIndex..<endIndex], radix: 16)
         }
     }
 }
